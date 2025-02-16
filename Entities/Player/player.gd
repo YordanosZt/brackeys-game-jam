2dbf244db0fa5de71b18036186e488b2916115b4
Yordanos <yordanoszt15@gmail.com>
2025-02-17 00:40:28 +0300

    Modify player controller

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -3,9 +3,12 @@ extends CharacterBody3D
 @export var move_speed: float = 500.0
 
 var input_dir: Vector2
-var direction: Vector3
+var direction: Vector2
 
 @onready var visuals: Node3D = %Visuals
+
+var isometric_up: Vector2 = Vector2(1, 1).normalized()
+var isometric_right: Vector2 = Vector2(1, -1).normalized()
 
 func _process(delta: float) -> void:
 	handle_input()
@@ -17,31 +20,19 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func handle_input() -> void:
-	var hor_input: float = Input.get_action_strength("right") - Input.get_action_strength("left")
-	var ver_input: float = Input.get_action_strength("down") - Input.get_action_strength("up")
-	
-	input_dir = Vector2(hor_input, ver_input).normalized()
-	
-	var angle: float = 15.0
-	var new_dir: Vector2
-	new_dir.x = cos(angle) * input_dir.x - sin(angle) * input_dir.y
-	new_dir.y = sin(angle) * input_dir.x + cos(angle) * input_dir.y
-	
-	input_dir = -new_dir
-	
-	#input_dir = Input.get_vector("left", "right", "up", "down")
+	input_dir = Input.get_vector("left", "right", "up", "down")
 
 func move(delta) -> void:
-	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = input_dir.x * isometric_right + input_dir.y * isometric_up
 	
 	if direction:
 		velocity.x = direction.x * move_speed * delta
-		velocity.z = direction.z * move_speed * delta
+		velocity.z = direction.y * move_speed * delta
 	else:
 		velocity.x = move_toward(velocity.x, 0, move_speed * delta)
 		velocity.z = move_toward(velocity.z, 0, move_speed * delta)
 
 func look() -> void:
 	if direction:
-		visuals.look_at(global_position - direction)
+		visuals.look_at(global_position - Vector3(direction.x, 0, direction.y))
 		visuals.rotation.x = 0
