a138ef0a46635b44e83559657f264973a46a64c7
Yordanos <yordanoszt15@gmail.com>
2025-02-19 15:50:14 +0300

    Add car spawn cleanup

--- Entities/Obstacles/Car/car.gd
+++ Entities/Obstacles/Car/car.gd
@@ -6,3 +6,7 @@ func _process(delta: float) -> void:
 	var move_vec = transform.basis.z * speed * delta
 	
 	global_translate(move_vec)
+
+
+func _on_hurt_box_area_entered(area: Area3D) -> void:
+	queue_free()
